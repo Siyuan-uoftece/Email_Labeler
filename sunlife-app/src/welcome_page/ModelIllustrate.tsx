f8154f028b1391b932827e50e38fe1f4e3cb3d44
Siyuan sharon Li <sharonsiyuan.li@mail.utoronto.ca>
2022-07-27 21:54:46 -0400

    fixed aos installation and enable scroll animation

--- sunlife-app/src/welcome_page/ModelIllustrate.tsx
+++ sunlife-app/src/welcome_page/ModelIllustrate.tsx
@@ -1,14 +1,14 @@
 import React, {useEffect} from "react";
-// import Aos from "aos";
-// import "aos/dist/aos.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import sun from "../assets/sunlife3.jpg"
 
 
 function ModelIllustrate() {
 
-    // useEffect(() => {
-    //     Aos.init({duration: 2000});
-    // }, []);
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, []);
     
     return(
         <div className="model" id="model">
@@ -22,7 +22,7 @@ function ModelIllustrate() {
 
             <div className="photos">
                 <div 
-                // data-aos="fade-up" 
+                data-aos="fade-up" 
                 className="pic"><img src="https://tenstorrent.com/app/uploads/2021/04/Group-66.png" /></div>
             </div>
 
@@ -31,3 +31,5 @@ function ModelIllustrate() {
 }
 
 export default ModelIllustrate
+
+
